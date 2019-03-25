17260c03b694cc4f1858890591cab13c3991ac40
Maks Pasichenko <maks23814300@gmail.com>
2019-03-25 17:52:22 +0200

    Add SQLite database and connected DB to the fragment SetCouplesAttendance

--- app/src/main/java/com/makspasich/journal/FragmentSetCouplesAttendance.java
+++ app/src/main/java/com/makspasich/journal/FragmentSetCouplesAttendance.java
@@ -1,6 +1,10 @@
 package com.makspasich.journal;
 
 import android.app.DatePickerDialog;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
@@ -8,12 +12,14 @@ import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.DateFormat;
@@ -28,34 +34,59 @@ public class FragmentSetCouplesAttendance extends Fragment {
     private RecyclerView rv;
     View view;
 
+
+
+    private SQLiteDatabase mDatabase;
+    private RVAdapterForSetCouplesAttendance mAdapter;
+    //private EditText mEditTextName;
+    //private TextView mTextViewAmount;
+    private int mAmount = 0;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_set_couples_attendance, container, false);
 
-        rv = view.findViewById(R.id.rv_set);
+        DBHelper dbHelper = new DBHelper(getActivity());
+        mDatabase = dbHelper.getWritableDatabase();
 
-        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
-        rv.setLayoutManager(llm);
-        rv.setHasFixedSize(true);
+        RecyclerView recyclerView = view.findViewById(R.id.rv_set);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mAdapter = new RVAdapterForSetCouplesAttendance(getActivity(), getAllItems());
+        recyclerView.setAdapter(mAdapter);
 
-        initializeData();
-        initializeAdapter();
+        Button buttonAdd = view.findViewById(R.id.temp_add_stud);
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addItem();
+            }
+        });
+
         return view;
     }
 
-    int totalSize = 30;
+    private void addItem() {
 
-    private void initializeData() {
-        persons = new ArrayList<>();
 
-        for (int i = 0; i < totalSize; i++) {
-            persons.add(new Person(i + ". FragmentSetCouplesAttendance"/*, i+"", R.drawable.ic_launcher_background*/));
-        }
+        String name = "Student";
+        ContentValues cv = new ContentValues();
+        cv.put(DataContract.DataEntry.COLUMN_STUDENT, name);
+
+        mDatabase.insert(DataContract.DataEntry.TABLE_NAME, null, cv);
+        mAdapter.swapCursor(getAllItems());
+
     }
-
-    private void initializeAdapter() {
-        RVAdapterForSetCouplesAttendance adapter = new RVAdapterForSetCouplesAttendance(persons);
-        rv.setAdapter(adapter);
+    private Cursor getAllItems() {
+        return mDatabase.query(
+                DataContract.DataEntry.TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
     }
 }
--- app/src/main/java/com/makspasich/journal/RVAdapterForSetCouplesAttendance.java
+++ app/src/main/java/com/makspasich/journal/RVAdapterForSetCouplesAttendance.java
@@ -1,25 +1,47 @@
 package com.makspasich.journal;
 
+import android.content.Context;
+import android.database.Cursor;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
 public class RVAdapterForSetCouplesAttendance extends RecyclerView.Adapter<RVAdapterForSetCouplesAttendance.PersonViewHolder> {
 
-    private List<Person> persons;
+    private Context mContext;
+    private Cursor mCursor;
+
+    public RVAdapterForSetCouplesAttendance(Context mContext, Cursor mCursor) {
+        this.mContext = mContext;
+        this.mCursor = mCursor;
+    }
 
     public static class PersonViewHolder extends RecyclerView.ViewHolder {
+        //ublic TextView nameText;
+        public ImageButton btnButton1;
+        public ImageButton btnButton2;
+        public Button btnButton3;
+
 
         CardView cv;
         TextView personName;
 
         PersonViewHolder(View itemView) {
             super(itemView);
+
+            this.btnButton1= itemView.findViewById(R.id.btn1);
+            this.btnButton2= itemView.findViewById(R.id.btn2);
+            this.btnButton3= itemView.findViewById(R.id.btn3);
+
             cv = itemView.findViewById(R.id.cv_set);
             personName = itemView.findViewById(R.id.person_name);
 
@@ -27,9 +49,6 @@ public class RVAdapterForSetCouplesAttendance extends RecyclerView.Adapter<RVAda
     }
 
 
-    RVAdapterForSetCouplesAttendance(List<Person> persons){
-        this.persons = persons;
-    }
 
     @Override
     public void onAttachedToRecyclerView(RecyclerView recyclerView) {
@@ -38,17 +57,60 @@ public class RVAdapterForSetCouplesAttendance extends RecyclerView.Adapter<RVAda
 
     @Override
     public PersonViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_for_set_co_att, viewGroup, false);
-        return new PersonViewHolder(v);
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View view = inflater.inflate(R.layout.item_for_set_co_att, viewGroup, false);
+        return new PersonViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(PersonViewHolder personViewHolder, int i) {
-        personViewHolder.personName.setText(persons.get(i).name);
+    public void onBindViewHolder(PersonViewHolder holder, final int position) {
+        if (!mCursor.moveToPosition(position)) {
+            return;
+        }
+
+        String name = mCursor.getString(mCursor.getColumnIndex(DataContract.DataEntry.COLUMN_STUDENT));
+        long id = mCursor.getLong(mCursor.getColumnIndex(DataContract.DataEntry._ID));
+
+        holder.personName.setText(name);
+        holder.itemView.setTag(id);
+
+
+        holder.btnButton1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.d("myLog", "Press button 1, posid"+position);
+            }
+        });
+
+        holder.btnButton2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.d("myLog", "Press button 2, posid"+position);
+            }
+        });
+
+        holder.btnButton3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.d("myLog", "Press button 3, posid"+position);
+            }
+        });
+
     }
 
     @Override
     public int getItemCount() {
-        return persons.size();
+        return mCursor.getCount();
+    }
+    public void swapCursor(Cursor newCursor) {
+        if (mCursor != null) {
+            mCursor.close();
+        }
+
+        mCursor = newCursor;
+
+        if (newCursor != null) {
+            notifyDataSetChanged();
+        }
     }
 }
